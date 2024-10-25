94ef5ce505e55f1e512ad9307c54ecf72ba2dcc0
AranApps <aaronfononi@gmail.com>
2024-10-25 18:01:56 +0330

    Add files via upload

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by AranApps on 28/08/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
